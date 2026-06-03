104299ff69ef00c74d866e6f2ae92ac28b954f22
Cristhian Silva Ramirez <cristhiangiovanysilvaramirez@gmail.com>
2026-06-03 16:30:42 -0500

    dynamic

--- src/app/editar/[id]/page.js
+++ src/app/editar/[id]/page.js
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const runtime = 'edge';
+
 'use client';
 import { useState, useEffect, use } from 'react';
 import { useRouter } from 'next/navigation';
